3f9b2686db71678cd63923a4ce17172680bb3701
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2023-06-27 18:44:15 +0200

    fix: :bug: refined deps

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.FRONT_URL,
+    optionsSuccessStatus: 200,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       disableErrorMessages: process.env.NODE_ENV === 'prod',
